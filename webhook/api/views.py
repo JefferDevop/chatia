04b5e4abd6bfe53392fbfbee48bf36676397d37f
JefferDevop <somadesarrollo@gmail.com>
2025-06-14 01:55:53 -0500

    Print get

--- webhook/api/views.py
+++ webhook/api/views.py
@@ -1,25 +1,22 @@
-# app_whatsapp/views.py
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-# @csrf_exempt
+@csrf_exempt
 def whatsapp_webhook(request):
     if request.method == 'GET':
-        # Verificación del webhook de WhatsApp
         print("GET request received for WhatsApp webhook")
         print("Request GET parameters:", request.GET)
-        
+
         mode = request.GET.get('hub.mode')
         token = request.GET.get('hub.verify_token')
         challenge = request.GET.get('hub.challenge')
+
         if mode == 'subscribe' and token == 'demo':
-            return JsonResponse({'hub.challenge': challenge})
-        return JsonResponse({}, status=403)
+            return HttpResponse(challenge, status=200)  # ✅ Texto plano
+        return HttpResponse("Forbidden", status=403)
 
     if request.method == 'POST':
         data = json.loads(request.body)
-        # Aquí guardarás o procesarás los mensajes entrantes
         print("Mensaje recibido:", data)
         return JsonResponse({'status': 'received'}, status=200)
-    
