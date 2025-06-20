488bff5cb9a7652adff9b757e3df14b44f60d0e0
JefferDevop <somadesarrollo@gmail.com>
2025-06-20 14:45:38 -0500

    Revision webhook v4

--- webhook/api/views.py
+++ webhook/api/views.py
@@ -55,7 +55,7 @@ class WhatsAppWebhookAPIView(APIView):
                             sender_name=sender_name,
                             message_body=texto,
                             created_at=timestamp,
-                            direction='IN'
+                            message_type='IN'
                         )
 
                         # Actualizar contacto
