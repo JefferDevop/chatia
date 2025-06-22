c86417ed5428b46f3fba2e2a4a15d5f402974f53
JefferDevop <somadesarrollo@gmail.com>
2025-06-21 20:00:18 -0500

    Cambio de estructura

--- core/asgi.py
+++ core/asgi.py
@@ -1,13 +1,13 @@
-# core/asgi.py
 import os
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from webhook.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
